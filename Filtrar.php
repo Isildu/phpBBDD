b5fbf897cbf40147ec1d1eaac5ec742564afaf27
Isildu <123159537+Isildu@users.noreply.github.com>
2025-03-24 12:26:33 +0000

    canvis com index i filtrar updae

--- Filtrar.php
+++ Filtrar.php
@@ -1,7 +1,24 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulari de Cerca</title>
+</head>
+<body>
+    <h1>Formulari per a cercar informació</h1>
+    
+    <form action="" method="GET">
+        <label for="seccio">Introdueix el nom de la noticia que ols trobar:</label>
+        <input type="text" id="seccio" name="seccio" required>
+        <button type="submit">Cercar</button>
+    </form>
+</body>
+</html>
 <?php
 $db = new SQLite3('diariLocal.db');
-    $resultats = $db->query("SELECT DISTINCT not_seccio FROM noticies ORDER BY not_seccio ASC");
+    $resultats = $db->query("SELECT * FROM noticies WHERE not_titular LIKE 'seccio' ORDER BY not_data DESC");
     while ($fila = $resultats->fetchArray(SQLITE3_ASSOC)) {
-        echo "<a href='veureNoticiesSeccio.php?nom=" . urlencode($fila['not_seccio']) . "'>" . htmlspecialchars($fila['not_seccio']) . "</a><br>";
+        echo "ID: " . $fila['not_id'] . " - Titular: " . $fila['not_titular'] . " - Cos: " . $fila['not_cos'] . " - Data: " . $fila['not_data'] . " - Secció: " . $fila['not_seccio'] . "<br>";
     }
 ?>
--- veureNoticiesSeccio.php
+++ veureNoticiesSeccio.php
@@ -1,8 +1,8 @@
 <?php
-$db = new SQLite3('diariLocal.db');
-$seccio = $_GET['seccio']
-$resultats = $db->query("SELECT * FROM noticies WHERE not_seccio='$seccio' ORDER BY not_seccio ASC");
-while ($fila = $resultats->fetchArray(SQLITE3_ASSOC)) {
-    echo "ID: " . $fila['not_id'] . " - Titular: " . $fila['not_titular'] . " - Cos: " . $fila['not_cos'] . " - Data: " . $fila['not_data'] . " - Secció: " . $fila['not_seccio'] . "<br>";
-}
+    $db = new SQLite3('diariLocal.db');
+    $seccio = $_GET['seccio'];
+    $resultats = $db->query("SELECT * FROM noticies WHERE not_seccio='$seccio' ORDER BY not_seccio ASC");
+    while ($fila = $resultats->fetchArray(SQLITE3_ASSOC)) {
+        echo "ID: " . $fila['not_id'] . " - Titular: " . $fila['not_titular'] . " - Cos: " . $fila['not_cos'] . " - Data: " . $fila['not_data'] . " - Secció: " . $fila['not_seccio'] . "<br>";
+    }
 ?>
