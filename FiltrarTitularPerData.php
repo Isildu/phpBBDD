d851396d4351eef075a37d85bc38d3a436c1aef5
Isildu <123159537+Isildu@users.noreply.github.com>
2025-03-24 12:58:43 +0000

    hg

---
+++ FiltrarTitularPerData.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulari de Cerca</title>
+</head>
+<body>
+    <h1>Formulari per a cercar informació</h1>
+    
+    <form action="" method="GET">
+        <label for="seccio">Introdueix el nom de la noticia que ols trobar:</label>
+        <input type="text" id="seccio" name="seccio" required>
+        <button type="submit">Cercar</button>
+    </form>
+    <?php
+    $contador = 0;
+    $seccio = $_GET['seccio'];
+    $db = new SQLite3('diariLocal.db');
+    $resultats = $db->query("SELECT * FROM noticies WHERE not_data LIKE '2025' ORDER BY not_data DESC");
+    while ($fila = $resultats->fetchArray(SQLITE3_ASSOC)) {
+        echo " - Titular: " . $fila['not_titular'] . " " . $fila['not_cos'] . " - Data: " . $fila['not_data'] . " - Secció: " . $fila['not_seccio'] . "<br>";
+        $contador ++;
+    }
+    if($contador == 0){
+        echo "ERROR NOTICIA NO TROBADA TORNI A PROVAR ESCRIVINT ALGUNA ALTRE COSA";
+    }
+?>
+</body>
+</html>
